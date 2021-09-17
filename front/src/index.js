e5e3083330f0ef0133364d7a1a79623c719dfaa0
lclpsoz <lclpsoz@gmail.com>
2021-09-17 19:05:51 -0300

    style(nubills): improve frontend folders organization

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Nubills from './Nubills';
+import Nubills from './pages/Nubills';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
