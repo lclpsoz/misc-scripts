40195f55ad2edd866948baab5c1a4dc057853b3a
lclpsoz <lclpsoz@gmail.com>
2021-09-18 16:34:33 -0300

    feat(nubills): sort by items without match by date if amount is the same
    translate to english button label
    stylled background collor into float elements that were transparent

--- front/src/pages/comps/NoMatch.js
+++ front/src/pages/comps/NoMatch.js
@@ -20,6 +20,10 @@ function compare(a, b) {
     return -1;
   if (a.amount < b.amount)
     return 1;
+  if (a.date.split('/').reverse().join('-') < b.date.split('/').reverse().join('-'))
+    return -1;
+  if (a.date.split('/').reverse().join('-') > b.date.split('/').reverse().join('-'))
+    return 1;
   return 0;
 }
 
@@ -40,12 +44,12 @@ function CardExpense(item, valSelect, valUnselect, valTotal, setSelect, setUnsel
 
   return (
     <Paper>
-      <Grid container spacing={1} onClick={handleChange}>
+      <Grid container spacing={1} onClick={handleChange} sx={{cursor: 'pointer'}}>
         <Grid item xs={12}>
           <Stack justifyContent='space-around' direction='row'>
-            <Chip label={date} />
-            <Chip label={category} />
-            <Chip label={`R$ ${(amount / 100).toFixed(2)}`} />
+            <Chip label={date}  sx={{cursor: 'pointer'}}/>
+            <Chip label={category}  sx={{cursor: 'pointer'}}/>
+            <Chip label={`R$ ${(amount / 100).toFixed(2)}`} sx={{cursor: 'pointer'}} />
           </Stack>
         </Grid>
         <Grid item xs={12} sx={{ textAlign: 'center' }}><ItemText elevation={0}>{title}</ItemText></Grid>
@@ -186,12 +190,17 @@ export default function NoMatch(props) {
             onChange={(event) => {
               props.setOpenMonth(event.target.value);
             }}
-            sx={{ width: '100px'}}
+            sx={{ width: '100px', background: 'white' }}
           />
-          <IconButton aria-label='save-open-month' direction='column' alignContent='flex-start' onClick={() => {
+          <IconButton
+            aria-label='save-open-month' direction='column'
+            alignContent='flex-start'
+            sx={{ background: 'white' }}
+            onClick={() => {
             setLoading(true);
             props.updateData().then(() => setLoading(false));
-          }}>
+          }}
+          >
             <SaveIcon />
           </IconButton>
         </Grid>
@@ -202,7 +211,7 @@ export default function NoMatch(props) {
             onClick={handleSubmit}
             endIcon={<SaveIcon />}
           >
-            Salvar matches
+            Save matches
           </Button>
         </Grid>
       </Grid>
