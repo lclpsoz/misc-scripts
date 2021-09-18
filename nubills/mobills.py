2b1dcdb39241c487f4481625b64d7bbe0a07cbe7
lclpsoz <lclpsoz@gmail.com>
2021-09-18 02:24:47 -0300

    feat(nubills): improve module mobills to return more organized data

--- nubills/mobills.py
+++ nubills/mobills.py
@@ -1,7 +1,7 @@
 import csv
 import glob
 import os
-from pprint import pprint
+import hashlib
 
 def get_mobills(info_file):
     mobills_csv_name = ''
@@ -13,9 +13,22 @@ def get_mobills(info_file):
 
     mobillsFilePath = os.path.join(mobills_csv_name)
     with open (mobillsFilePath, 'r') as csvFile:
-        mobills = list(csv.reader (csvFile, delimiter=';'))[1:]
-        for i in range (len (mobills)):
-            mobills[i][3] = int (''.join(mobills[i][3].split (' ')[1].split(',')).replace('.', ''))
+        mobills_data_table = list(csv.reader (csvFile, delimiter=';'))[1:]
+        for i in range (len (mobills_data_table)):
+            mobills_data_table[i][3] = int (''.join(mobills_data_table[i][3].split (' ')[1].split(',')).replace('.', ''))
 
-    print('Returning mobills data!')
-    return mobills
+    mobills_data = {}
+    for exp_row in mobills_data_table:
+        exp_id = hashlib.md5(str.encode(str(exp_row))).hexdigest()
+        if exp_id in mobills_data:
+            mobills_data[exp_id]['count'] += 1
+        else:
+            mobills_data[exp_id] = {
+                'date': exp_row[0],
+                'title': exp_row[1],
+                'category': exp_row[2],
+                'amount': exp_row[3],
+                'count': 1
+            }
+
+    return mobills_data
