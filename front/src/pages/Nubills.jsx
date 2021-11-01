09fec0fc2c59089c305f60cd8f96aa4f790d4416
lclpsoz <lclpsoz@gmail.com>
2021-11-01 01:06:39 -0300

    refactor(nubills): move loading and open-month to Nubills module

--- front/src/pages/Nubills.jsx
+++ front/src/pages/Nubills.jsx
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Box, Stack } from '@mui/material';
+import {
+  Box, Stack, Grid, TextField, IconButton,
+  Backdrop, CircularProgress
+} from '@mui/material';
+import { Save as SaveIcon } from '@mui/icons-material'
 import Matches from './comps/matches';
 import NoMatch from './comps/noMatch';
 
@@ -10,6 +14,8 @@ function Nubills() {
   const [nubankNoMatch, setNubankNoMatch] = useState(undefined);
 
   const [openMonth, setOpenMonth] = useState('');
+
+  const [loading, setLoading] = useState(false);
 
   const updateData = () => axios.get(process.env.REACT_APP_NODE, { params: { openMonth, mobillsFileName: '' } }).then((res) => {
     // Set minimum amount of cards to show in Matches
@@ -38,18 +44,66 @@ function Nubills() {
   }, []);
 
   return (
-    <Box sx={{ maxWidth: '1500px', margin: '0 auto' }}>
-      <Stack direction="row" spacing={3} justifyContent="space-around">
-        <Matches matches={matches} setMatches={setMatches} />
-        <NoMatch
-          mobillsNoMatch={mobillsNoMatch}
-          nubankNoMatch={nubankNoMatch}
-          updateData={updateData}
-          openMonth={openMonth}
-          setOpenMonth={setOpenMonth}
-        />
-      </Stack>
-    </Box>
+    <>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color='inherit' />
+      </Backdrop>
+
+      <Box sx={{ maxWidth: '1500px', margin: '0 auto' }}>
+        <Stack direction="row" spacing={3} justifyContent="space-around">
+          <Matches matches={matches} setMatches={setMatches} />
+          <NoMatch
+            mobillsNoMatch={mobillsNoMatch}
+            nubankNoMatch={nubankNoMatch}
+            updateData={updateData}
+            openMonth={openMonth}
+            setOpenMonth={setOpenMonth}
+            loading={loading}
+            setLoading={setLoading}
+          />
+        </Stack>
+      </Box>
+
+      <Grid container justifyContent='center' alignItems='center' spacing={1}
+        sx={{
+          margin: 0,
+          top: 'auto',
+          right: 'auto',
+          bottom: 20,
+          left: '0',
+          position: 'fixed',
+          zIndex: 1,
+          pointerEvents: 'none'
+        }}
+      >
+        <Grid item container xs={12} direction='row' justifyContent='center' alignItems='center'>
+          <TextField
+            id='open-month'
+            label='Open month'
+            variant='filled'
+            value={openMonth}
+            onChange={(event) => {
+              setOpenMonth(event.target.value);
+            }}
+            sx={{ width: '100px', background: 'white', pointerEvents: 'auto' }}
+          />
+          <IconButton
+            aria-label='save-open-month' direction='column'
+            alignContent='flex-start'
+            sx={{ background: 'white', pointerEvents: 'auto' }}
+            onClick={() => {
+              setLoading(true);
+              updateData().then(() => setLoading(false));
+            }}
+          >
+            <SaveIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
